76e7092f2cff77bb594a17003f26096fe2efe0dd
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-06-13 16:03:17 +0200

    Updated urls.py for django 1.8

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns(
+urlpatterns = [
     '',
-    # Examples:
-    # url(r'^$', 'omnidia.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', 'omnidia.views.home'),
-)
+]
 
-urlpatterns += patterns(
+urlpatterns += [
     '',
     url(r'^$', 'omnidia.views.home'),
-)
+]
