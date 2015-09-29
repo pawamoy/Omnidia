4334d481da03798fd0ceb858c7adef0e2e27892f
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-09-29 21:31:28 +0200

    Stuff

--- omnidia/admin.py
+++ omnidia/admin.py
@@ -7,9 +7,41 @@ from omnidia.models import (
     ModelField, ObjectDatasetValue, ObjectValue)
 
 
-admin.site.register(Dataset)
-admin.site.register(DatasetValue)
-admin.site.register(FileType)
+class DatasetValueInline(admin.TabularInline):
+    model = DatasetValue
+    extra = 0
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'datatype')
+    list_editable = ('name', 'datatype')
+    inlines = (DatasetValueInline, )
+
+
+class DatasetValueAdmin(admin.ModelAdmin):
+    list_display = ('dataset', 'value')
+    list_editable = ('value', )
+
+
+class FileTypeFieldInline(admin.TabularInline):
+    model = FileTypeField
+    extra = 0
+
+
+class FileTypeDatasetFieldInline(admin.TabularInline):
+    model = FileTypeDatasetField
+    extra = 0
+
+
+class FileTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_editable = ('name', )
+    inlines = (FileTypeFieldInline, FileTypeDatasetFieldInline)
+
+
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(DatasetValue, DatasetValueAdmin)
+admin.site.register(FileType, FileTypeAdmin)
 admin.site.register(File)
 admin.site.register(FileTypeDatasetField)
 admin.site.register(FileTypeField)
